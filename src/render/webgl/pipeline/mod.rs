8cb2aab7c19426f6a6dd0d83de3c92915b49a76d
MagicCrazyMan <350088648@qq.com>
2024-01-03 19:57:00 +0800

    wip

--- src/render/webgl/pipeline/mod.rs
+++ src/render/webgl/pipeline/mod.rs
@@ -26,48 +26,51 @@ pub fn create_standard_pipeline(
     let composer = ItemKey::from_uuid();
 
     let collected_entities = ResourceKey::new_runtime_uuid();
-    // let picked_entity = ResourceKey::new_runtime_uuid();
-    // let picked_position = ResourceKey::new_runtime_uuid();
-    // let outline_texture = ResourceKey::new_runtime_uuid();
+    let picked_entity = ResourceKey::new_runtime_uuid();
+    let picked_position = ResourceKey::new_runtime_uuid();
+    let outline_texture = ResourceKey::new_runtime_uuid();
     let standard_draw_texture = ResourceKey::new_runtime_uuid();
-    // let gaussian_blur_texture = ResourceKey::new_runtime_uuid();
+    let gaussian_blur_texture = ResourceKey::new_runtime_uuid();
 
     let mut pipeline = Pipeline::new();
     pipeline.add_executor(
         collector.clone(),
         StandardEntitiesCollector::new(collected_entities.clone()),
     );
-    // pipeline.add_executor(
-    //     picking.clone(),
-    //     Picking::new(
-    //         collected_entities.clone(),
-    //         in_window_position,
-    //         picked_entity.clone(),
-    //         picked_position.clone(),
-    //     ),
-    // );
-    // pipeline.add_executor(
-    //     outlining.clone(),
-    //     Outlining::new(picked_entity, outline_texture.clone()),
-    // );
-    // pipeline.add_executor(
-    //     gaussian_blur.clone(),
-    //     GaussianBlur::new(outline_texture, gaussian_blur_texture.clone()),
-    // );
+    pipeline.add_executor(
+        picking.clone(),
+        Picking::new(
+            collected_entities.clone(),
+            in_window_position,
+            picked_entity.clone(),
+            picked_position.clone(),
+        ),
+    );
+    pipeline.add_executor(
+        outlining.clone(),
+        Outlining::new(picked_entity, outline_texture.clone()),
+    );
+    pipeline.add_executor(
+        gaussian_blur.clone(),
+        GaussianBlur::new(outline_texture, gaussian_blur_texture.clone()),
+    );
     pipeline.add_executor(
         drawer.clone(),
         StandardDrawer::new(collected_entities, standard_draw_texture.clone()),
     );
     pipeline.add_executor(
         composer.clone(),
-        StandardComposer::new(vec![standard_draw_texture], in_clear_color),
+        StandardComposer::new(
+            vec![standard_draw_texture, gaussian_blur_texture],
+            in_clear_color,
+        ),
     );
 
     // safely unwraps
-    // pipeline.connect(&collector, &picking).unwrap();
-    // pipeline.connect(&picking, &outlining).unwrap();
-    // pipeline.connect(&outlining, &gaussian_blur).unwrap();
-    // pipeline.connect(&gaussian_blur, &composer).unwrap();
+    pipeline.connect(&collector, &picking).unwrap();
+    pipeline.connect(&picking, &outlining).unwrap();
+    pipeline.connect(&outlining, &gaussian_blur).unwrap();
+    pipeline.connect(&gaussian_blur, &composer).unwrap();
     pipeline.connect(&collector, &drawer).unwrap();
     pipeline.connect(&drawer, &composer).unwrap();
 
